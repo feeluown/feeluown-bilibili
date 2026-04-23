e175e02ba4ce820147a98890a1285115b2ccd0bf
cosven <yinshaowen241@gmail.com>
2026-04-23 10:29:19 +0800

    bump version 0.5.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'feeluown-bilibili',
-    'version': '0.5.4',
+    'version': '0.5.5',
     'description': 'Bilibili provider for FeelUOwn player.',
     'author': 'Bruce Zhang',
     'url': 'https://github.com/feeluown/feeluown-bilibili',
